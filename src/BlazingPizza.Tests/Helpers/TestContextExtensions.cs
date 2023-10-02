81e7481b0fbc9ba8886cdf8d962e2ea89b838fba
rwaldron <outsideinprofits@gmail.com>
2023-10-02 22:09:49 +1100

    chore: :tada: Initial commit
    starter from MS Blazor Workshop

---
+++ src/BlazingPizza.Tests/Helpers/TestContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BlazingPizza.Client;
+using BlazingPizza.Client.Services;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazingPizza
+{
+    public static class TestContextExtensions
+    {
+        public static TestContext AddBlazingPizzaSupport(this TestContext context)
+        {
+            context.Services.AddSingleton<FakePizzaApi>(new FakePizzaApi());
+            context.Services.AddSingleton<IPizzaApi>(s => s.GetRequiredService<FakePizzaApi>());
+
+            return context;
+        }
+    }
+}
